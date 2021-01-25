693b7eb57fed011c4eac1bc95ac10df3bcb0edfb
AksenovKirill <aksenovkirill@yandex.ru>
2021-01-26 00:56:38 +0300

    Авторизация

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,10 @@
-var navMain = document.querySelector(".navigation-main");
-var navToggle = document.querySelector(".navigation-main__toggle");
 
-navMain.classList.remove("navigation-main");
+var navToggle = document.querySelector(".burger");
+var navMain = document.querySelector(".navigation-main");
+
+navMain.classList.remove("navigation-main--opened");
 
 navToggle.addEventListener("click", function () {
-  if (navMain.classList.contains("navigation-main--closed")) {
-    navMain.classList.remove("navigation-main--closed");
-    navMain.classList.add("navigation-main--opened");
-  } else {
-    navMain.classList.add("navigation-main--closed");
-    navMain.classList.remove("navigation-main--opened");
-  }
+  navMain.classList.toggle("navigation-main--opened");
+  navToggle.classList.toggle("burger--closed");
 });
-
